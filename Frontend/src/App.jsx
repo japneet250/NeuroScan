df97b804ba24471643bb1a572cedc39a1cd28170
japneet250 <singj93@mcmaster.ca>
2024-11-16 04:08:56 -0500

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Who from "./components/Who";
 import Works from "./components/Works";
-import Upload from "./components/Upload";
+import Upload from "./components/upload";
 
 const Container = styled.div`
   height: 100vh;
